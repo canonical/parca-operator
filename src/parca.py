d3a70e7a96acefd6c319eb464b229e111dee1a26
Jon Seager <jon@sgrs.uk>
2023-02-16 09:32:02 +0000

    feat: use snap from `latest/stable`

--- src/parca.py
+++ src/parca.py
@@ -20,7 +20,7 @@ class Parca:
     def install(self):
         """Install the Parca snap package."""
         try:
-            self._snap.ensure(snap.SnapState.Latest, channel="edge")
+            self._snap.ensure(snap.SnapState.Latest, channel="stable")
             snap.hold_refresh()
         except snap.SnapError as e:
             logger.error("could not install parca. Reason: %s", e.message)
